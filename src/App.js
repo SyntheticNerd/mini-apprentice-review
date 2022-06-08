3c5ad33a506cebe99d3e89981c547784594dc86d
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-06-08 15:40:38 -0700

    cleaning up uneeded things

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-import Footer from "./components/footer/Footer";
-import Hero from "./components/Hero";
-import Nav from "./components/Nav";
-import Testimonials from "./components/testimonials/Testimonials";
-
 function App() {
   return (
     <div className='App'>
