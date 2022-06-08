0a772bacdd9bac20da252b6d49ffe24b91d0ddb7
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-06-08 15:38:57 -0700

    reset main branch to framework

--- src/App.js
+++ src/App.js
@@ -6,12 +6,55 @@ import Testimonials from "./components/testimonials/Testimonials";
 function App() {
   return (
     <div className='App'>
-      {/* We see that there are four major elements to the page, the nav, footer, hero section and testimonials.
-      Each of these can and be made into their own components. */}
-      <Nav />
-      <Hero />
-      <Testimonials />
-      <Footer />
+      Our Work
+      About Us
+      Contact
+
+      Weber Ltd
+      Enterprise-Wide User-Facing Success
+      SEE OUR WORK
+
+      Daniel Clifford
+      Verified Graduate
+      I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a
+      transition and have heard some people who had an amazing experience here. I signed up
+      for the free intro course and found it incredibly fun! I enrolled shortly thereafter.
+      The next 12 weeks was the best - and most grueling - time of my life. Since completing
+      the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup.
+
+
+      Jonathan Walters
+      Verified Graduate,
+      I started as a total newbie with virtually no coding skills. I now work as a mobile engineer
+      for a big company. This was one of the best investments I’ve made in myself.
+
+      Kira Whittle
+      Verified Graduate
+      Before joining the bootcamp, I’ve never written a line of code. I needed some structure from
+      professionals who can help me learn programming step by step. I was encouraged to enroll by a former
+      student of theirs who can only say wonderful things about the program. The entire curriculum and staff
+      did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team
+      project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial
+      could ever have. In fact, I’ve often referred to it during interviews as an example of my developent
+      experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers.
+      100% recommend!
+
+    
+      Jeanette Harmon
+      Verified Graduate
+      Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living
+      while doing something I love.
+    
+      Patrick Abrams
+      Verified Graduate
+      The staff seem genuinely concerned about my progress which I find really refreshing. The program
+      gave me the confidence necessary to be able to go out in the world and present myself as a capable
+      junior developer. The standard is above the rest. You will get the personal attention you need from
+      an incredible community of smart and amazing people.
+
+      Our Work
+      About Us
+      Contact
     </div>
   );
 }
